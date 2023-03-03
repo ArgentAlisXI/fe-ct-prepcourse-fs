99c53f3f4fcbcfae66ab3ff6425d55dbcbd95122
Matias Rios <mati98rios@hotmail.com>
2023-03-03 15:44:14 -0300

    Ejercicios resueltos

--- M04 JavaScript Bucles/homework.js
+++ M04 JavaScript Bucles/homework.js
@@ -4,7 +4,9 @@ function obtenerMayor(x, y) {
    // "x" e "y" son números enteros.
    // Retornar el número más grande.
    // Si son iguales, retornar cualquiera de los dos.
-   // Tu código:
+   if (x == y) {
+      return XPathResult(x == y)
+   }
 }
 
 function mayoriaDeEdad(edad) {
